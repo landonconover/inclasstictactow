caa48540f00bf324ba256ed7b76f4946761621d4
Landon Conover <landon.conover@gmail.com>
2022-03-14 17:09:47 -0600

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,110 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+function Square({ value, onClick }) {
+
+  return (
+    <button className='square' onClick={onClick}>
+      {value}
+    </button>
+  )
+}
+
+function Game() {
+
+  const [ squares, setSquares ] = useState(Array(9).fill(null));
+  const [ isXNext, setIsXNext ] = useState(true)
+
+  const winner = calculateWinner(squares);
+
+  function renderSquare(i) {
+    return <Square
+      value={squares[i]}
+      onClick={() => {
+
+        if(squares[i] !== null || winner !== null) {
+          return
+        }
+
+        const nextSquares = squares.slice();
+        nextSquares[i] = isXNext ? "X" : "O"
+        setSquares(nextSquares)
+
+        setIsXNext(!isXNext)
+      }}
+    />;
+  }
+
+  function getStatus() {
+    if (winner) {
+      return "Winner: " + winner;
+    } else if (isBoardFull(squares)) {
+      return "Draw!";
+    } else {
+      return "Next player: " + (isXNext ? "X" : "O");
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="game">
+        <div className="game-board">
+          <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+        </div>
+        <div className="game-info">{getStatus()}</div>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const possibleLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  // go over all possibly winning lines and check if they consist of only X's/only O's
+  for (let i = 0; i < possibleLines.length; i++) {
+    const [a, b, c] = possibleLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function isBoardFull(squares) {
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i] == null) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+ReactDOM.render(<Game />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
